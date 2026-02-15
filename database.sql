abce836563412560d955529862a751259d09c0b6
Albina787 <c.hasan.albina@student.uzhnu.edu.ua>
2026-02-15 19:29:30 +0200

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,1 +1,13 @@
+CREATE TABLE articles (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255),
+    content TEXT
+);
+
+CREATE TABLE comments (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    article_id INT,
+    text TEXT,
+    FOREIGN KEY (article_id) REFERENCES articles(id)
+);
 
