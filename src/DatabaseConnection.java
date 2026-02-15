b1df1e4bf1c75ede96e2672677b726f80a474f45
Albina787 <c.hasan.albina@student.uzhnu.edu.ua>
2026-02-15 19:25:24 +0200

    Update DatabaseConnection.java

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -1,1 +1,14 @@
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DatabaseConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/blogdb";
+    private static final String USER = "root";
+    private static final String PASSWORD = "password";
+
+    public static Connection getConnection() throws Exception {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
