d63534e4a57ff08f9d65200413c8f261ccecbbae
Albina787 <c.hasan.albina@student.uzhnu.edu.ua>
2026-02-15 19:26:25 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,22 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        // CREATE
+        Article article = new Article("My First Article", "Active Record pattern example.");
+        article.save();
+
+        // READ
+        Article found = Article.findById(article.getId());
+
+        // UPDATE
+        found.update();
+
+        // CREATE COMMENT
+        Comment comment = new Comment(article.getId(), "Great article!");
+        comment.save();
+
+        // DELETE
+        // found.delete();
+    }
+}
 
